5a675cddba27e68cc6226126ca4d4ea370a337e4
shravangm <shravan.gm25@gmail.com>
2021-09-11 23:17:40 +0530

    Committing after altering as per leads comments

--- src/test/java/frameworkCvC/Test1.java
+++ src/test/java/frameworkCvC/Test1.java
@@ -1,11 +1,22 @@
 package frameworkCvC;
 
-import org.testng.annotations.Test;
+import static org.testng.Assert.assertEquals;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+/**
+ * 
+ * @author pc
+ * This is to test the Git practice
+ */
 public class Test1 {
+	/*
+	 *This method is to execute Git test method
+	 */
 	@Test
-	public void GitTest() {
+	public void GitTest() {		 
+		Assert.assertEquals(true, true);
 		System.out.println("Execute Git test");
-	}
+		}
 	
 }
